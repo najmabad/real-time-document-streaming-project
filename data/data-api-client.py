cffd8488b128f5c878595e53c08ef8405321c604
najmabader <najmabader@hotmail.it>
2022-01-16 23:24:45 +0100

    apply formatting

--- data/data-api-client.py
+++ data/data-api-client.py
@@ -1,17 +1,18 @@
 import json
+
 import requests
 
-with open('./output.txt') as f:
+with open("./output.txt") as f:
     for line in f:
         line_json = json.loads(line)
-        response = requests.post('http://localhost:80/invoiceitem', json=line_json)
+        response = requests.post("http://localhost:80/invoiceitem", json=line_json)
 
         if response.status_code != 201:
-            print("*"*80)
+            print("*" * 80)
             print("Original line:")
             print(line_json)
-            print("*"*80)
+            print("*" * 80)
             print("Response:")
             print(response.json())
-            print("*"*80)
-            print("*"*80)
+            print("*" * 80)
+            print("*" * 80)
